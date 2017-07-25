2022c04ca6dcf1939c8a94215b31cdb284db936a
Derrick Ross <d_offial@yahoo.com>
2017-07-25 16:20:23 -0400

    Finished adding HTML, started adding google maps api with styling/JS

---
+++ UnitedTrans.js
@@ -0,0 +1,22 @@
+function mainSlider(onload) {
+
+}
+
+function toTopBtn(onclick) {
+
+}
+
+function functionName() {
+
+}
+
+function initMap() {
+  let uluru = {lat:26.5610038 , lng:-81.8543355 };
+  let map = new google.maps.Map(document.getElementById("map"), {
+    zoom: 4,
+    center: uluru,
+  });
+}
+
+async defer
+src="https://maps.googleapis.com/maps/api/js?key= &callback=initMap";

e8d66732e5bb4d7742ed11221c69727160edbc3c
Derrick Ross <d_offial@yahoo.com>
2017-07-26 16:43:17 -0400

    Added map, and began adding jquery for smooth scrolling

--- UnitedTrans.js
+++ UnitedTrans.js
@@ -1,22 +1,52 @@
-function mainSlider(onload) {
-
-}
-
-function toTopBtn(onclick) {
-
-}
-
-function functionName() {
-
-}
-
+//google map
 function initMap() {
-  let uluru = {lat:26.5610038 , lng:-81.8543355 };
-  let map = new google.maps.Map(document.getElementById("map"), {
-    zoom: 4,
-    center: uluru,
+  let center = {lat: 26.561 , lng: -81.85216109999999 };
+  let map = new google.maps.Map(document.getElementById("map"), { zoom: 15, center: center});
+  let marker = new google.maps.Marker({ position:{lat: 26.561,lng:-81.85216109999999}, map:map});
+
+let infoWindow = new google.maps.InfoWindow({
+  content: '<h4>United Transmission Sercives Inc.</h4><address>12541 Metro Pkwy #15<Fort Myers, FL 33966</address>'
   });
+
+marker.addListener('click', function(){
+  infoWindow.open(map, marker);
+  });
+
 }
 
-async defer
-src="https://maps.googleapis.com/maps/api/js?key= &callback=initMap";
+// Select all links with hashes
+$('a[href*="#"]')
+  // Remove links that don't actually link to anything
+  .not('[href="#"]')
+  .not('[href="#0"]')
+  .click(function(event) {
+    // On-page links
+    if (
+      location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '')
+      &&
+      location.hostname == this.hostname
+    ) {
+      // Figure out element to scroll to
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+      // Does a scroll target exist?
+      if (target.length) {
+        // Only prevent default if animation is actually gonna happen
+        event.preventDefault();
+        $('html, body').animate({
+          scrollTop: target.offset().top
+        }, 1000, function() {
+          // Callback after animation
+          // Must change focus!
+          var $target = $(target);
+          $target.focus();
+          if ($target.is(":focus")) { // Checking if the target was focused
+            return false;
+          } else {
+            $target.attr('tabindex','-1'); // Adding tabindex for elements not focusable
+            $target.focus(); // Set focus again
+          };
+        });
+      }
+    }
+  });
